c68fd3b828525beb8fb03e16ce6593998453705a
CknightX <caokeck123@163.com>
2022-04-20 17:58:56 +0800

    bug fix

--- lua/configs/autocomplete.lua
+++ lua/configs/autocomplete.lua
@@ -17,19 +17,13 @@ function M.config()
             -- vim.fn["UltiSnips#Anon"](args.body)
             end,
         },
-        mapping = {
-            ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-            ['<C-y>'] = cmp.config.disable,
-            ['<C-e>'] = cmp.mapping({
-                i = cmp.mapping.abort(),
-                c = cmp.mapping.close(),
-            }),
-            -- Accept currently selected item...
-            -- Set `select` to `false` to only confirm explicitly selected items:
-            ['<CR>'] = cmp.mapping.confirm({ select = true }), 
-        },
+        mapping = cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
         sources = cmp.config.sources({
             { name = 'nvim_lsp' },
             -- { name = 'luasnip' }, -- For luasnip users.
